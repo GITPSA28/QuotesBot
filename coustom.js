73a823eb46ffdf31d37ffb3b977bb48ab3e23784
GITPSA28 <74608471+GITPSA28@users.noreply.github.com>
2021-11-13 21:55:15 +0530

    colorpicker added

--- coustom.js
+++ coustom.js
@@ -1,6 +1,16 @@
 const defaultBtn = document.querySelector("#default-btn");
 const customBtn = document.querySelector("#custom-btn");
 const img = document.querySelector("#bgimage");
+const quotetext=document.querySelector('#quote');
+var colorPicker;
+var defaultColor = "#ffffff";
+colorPicker = document.querySelector("#colorPicker");
+colorPicker.value = defaultColor;
+colorPicker.addEventListener("input", updateFirst, false);
+colorPicker.select();
+function updateFirst(event) {
+    quotetext.style.color = event.target.value;
+  }
 customBtn.addEventListener('click',defaultBtnActive);
 function defaultBtnActive(){
            defaultBtn.click();
